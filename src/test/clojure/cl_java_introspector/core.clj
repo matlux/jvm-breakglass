e2d0cf3906d5cc382e052ca608f1435933e584b4
Mathieu Gauthron <mathieu@matlux.net>
2013-03-07 18:49:38 +0000

    replaced old obj2map

--- src/test/clojure/cl_java_introspector/core.clj
+++ src/test/clojure/cl_java_introspector/core.clj
@@ -30,7 +30,7 @@
     (obj2methods obj)))
 ;(get-obj-methods "")
 
-(defn obj2map-level [obj level]
+(defn obj2map [obj level]
   (let [obj2fields (fn [obj] (map #(do (.setAccessible % true) %) (into [] (. (. obj getClass) getDeclaredFields))))
         get-inst-fields (fn [fields] (filter #(not (Modifier/isStatic (.getModifiers %))) fields))
         field2ref (fn [field obj] (.get field obj))
@@ -47,22 +47,7 @@
           :else (reduce #(assoc %1 (.getName %2) (if (zero? level) (field2ref %2 obj) (obj2map (field2ref %2 obj) (dec level)))) {} (get-inst-fields (obj2fields obj))) )
     ))
 
-(defn obj2map [obj]
-  (let [obj2fields (fn [obj] (map #(do (.setAccessible % true) %) (into [] (. (. obj getClass) getDeclaredFields))))
-        get-inst-fields (fn [fields] (filter #(not (Modifier/isStatic (.getModifiers %))) fields))
-        field2ref (fn [field obj] (.get field obj))
-        ]
-                                        ;(reduce #(assoc %1 (.getName %2) (field2value %2)) {} in-fields)
-    (cond (nil? obj) nil
-          (instance? java.lang.String obj) obj
-          (instance? java.lang.Number obj) obj
-          (instance? java.lang.Iterable obj) (into [] (map (fn [e] (obj2map e)) (into [] obj)))
-          (instance? java.util.Map obj) (let [m (into {} obj)
-                                              ks (keys m)
-                                              ]
-                                          (reduce #(assoc %1 %2 (obj2map (m %2))) {} ks))
-          :else (reduce #(assoc %1 (.getName %2) (obj2map (field2ref %2 obj))) {} (get-inst-fields (obj2fields obj))) )
-    ))
+
 
 (defn remote-execute [hostname port code]
   (try
