15200b75e46e0b3f7e8c37226a7465a22b399c93
Mathieu Gauthron <mathieu@matlux.net>
2014-05-29 22:10:19 +0100

    added spring webapp example

---
+++ Spring3MVC/src/main/java/net/matlux/testobjects/Employee.java
@@ -0,0 +1,29 @@
+package net.matlux.testobjects;
+
+public class Employee {
+	
+	private String firstname;
+	private String lastname;
+	
+	private Address address;
+	
+	public Employee(String firstname, String lastname, Address address) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.setAddress(address);
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
