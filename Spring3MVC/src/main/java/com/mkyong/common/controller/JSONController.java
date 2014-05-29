b7ae5565f1c82b44801c8471d40da91436f33ccb
Mathieu Gauthron <mathieu@matlux.net>
2014-05-29 22:55:12 +0100

    got the Spring3MVC to display employee business data

--- Spring3MVC/src/main/java/com/mkyong/common/controller/JSONController.java
+++ Spring3MVC/src/main/java/com/mkyong/common/controller/JSONController.java
@@ -1,17 +1,26 @@
 package com.mkyong.common.controller;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import net.matlux.NreplServer;
+import net.matlux.testobjects.Department;
+import net.matlux.testobjects.Employee;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.mkyong.common.model.Shop;
 
 @Controller
 @RequestMapping("/kfc/brands")
 public class JSONController {
 
-	@RequestMapping(value = "{name}", method = RequestMethod.GET)
+	/*@RequestMapping(value = "{name}", method = RequestMethod.GET)
 	public @ResponseBody
 	Shop getShopInJSON(@PathVariable String name) {
 
@@ -20,6 +29,29 @@ public class JSONController {
 		shop.setStaffName(new String[] { "mkyong1", "mkyong2" });
 
 		return shop;
+
+	}*/
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "{name}", method = RequestMethod.GET)
+	public @ResponseBody
+	Department getEmployeeFromNY(@PathVariable String name) {
+
+		Department dep = (Department)NreplServer.instance.get("department");
+		List<Employee> emps = dep.getEmployees();
+		Collections.sort(emps,new Comparator() {
+
+			@Override
+			public int compare(Object o1, Object o2) {
+				Employee e1 = (Employee)o1;
+				Employee e2 = (Employee)o2;
+				return e1.getAddress().getCity().compareTo(e2.getAddress().getCity());
+			}
+			
+		});
+		
+		return dep;
+
 
 	}
 
