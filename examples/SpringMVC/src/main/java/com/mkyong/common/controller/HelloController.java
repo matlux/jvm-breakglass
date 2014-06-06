4caafd5e1448444cb1044e4b6fa3e0e909b05a33
Mathieu Gauthron <mathieu@matlux.net>
2014-06-06 13:26:39 +0100

    finally added SpringMVC helloworld example

---
+++ examples/SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.mkyong.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "hello";
+
+	}
+	
+}
