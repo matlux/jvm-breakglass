d1f25fef5b4392b0f884f0dc163bf938ff974a80
Mathieu Gauthron <mathieu@matlux.net>
2014-06-06 17:39:51 +0100

    modified SpringMVC to get example to work (missing files)

--- examples/SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
+++ examples/SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
@@ -1,17 +1,49 @@
 package com.mkyong.common.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import net.matlux.NreplServer;
+import net.matlux.testobjects.Department;
+import net.matlux.testobjects.Employee;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("/city")
 public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
+		String name = "NY";
+		Department dep = (Department)NreplServer.instance.get("department");
+		List<Employee> emps = dep.getEmployees();
+		Collections.sort(emps,new Comparator() {
 
+			@Override
+			public int compare(Object o1, Object o2) {
+				Employee e1 = (Employee)o1;
+				Employee e2 = (Employee)o2;
+				return e1.getAddress().getCity().compareTo(e2.getAddress().getCity());
+			}
+			
+		});
+		List<Employee> empsRes = new ArrayList<Employee>();
+		for(Employee e:emps) {
+			if(name.equals("all")) {
+				empsRes.add(e);
+			} else if(name.equalsIgnoreCase(e.getAddress().getCity()) ) {
+				empsRes.add(e);
+			}
+			
+		}
+		
+		model.put("empsRes", empsRes);
 		model.addAttribute("message", "Spring 3 MVC Hello World");
 		return "hello";
 
--- examples/SpringMVC/src/main/webapp/WEB-INF/pages/hello.jsp
+++ examples/SpringMVC/src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-	<h1>Message : ${message}</h1>	
+	<h1>Report: Employees living in NY</h1>	
+	<h2>List : ${empsRes}</h2>	
 </body>
 </html>
