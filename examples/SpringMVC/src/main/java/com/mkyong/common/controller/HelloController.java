274333b2c3a7a6150e8f49d8b18b08695f44e967
Mathieu Gauthron <mathieu@matlux.net>
2014-06-07 17:41:01 +0100

    added get all employees webpage

--- examples/SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
+++ examples/SpringMVC/src/main/java/com/mkyong/common/controller/HelloController.java
@@ -50,5 +50,35 @@ public class HelloController {
 		return "hello";
 
 	}
+	@RequestMapping(method = RequestMethod.GET)
+	public String getAllEmployees(ModelMap model) {
+		String name = "all";
+		Department dep = (Department)NreplServer.instance.get("department");
+		List<Employee> emps = dep.getEmployees();
+		Collections.sort(emps,new Comparator() {
+
+			@Override
+			public int compare(Object o1, Object o2) {
+				Employee e1 = (Employee)o1;
+				Employee e2 = (Employee)o2;
+				return e1.getAddress().getCity().compareTo(e2.getAddress().getCity());
+			}
+			
+		});
+		List<Employee> empsRes = new ArrayList<Employee>();
+		for(Employee e:emps) {
+			if(name.equals("all")) {
+				empsRes.add(e);
+			} else if(name.equalsIgnoreCase(e.getAddress().getCity()) ) {
+				empsRes.add(e);
+			}
+			
+		}
+		
+		model.put("empsRes", empsRes);
+		model.put("city", name);
+		return "displayAllEmployees";
+
+	}
 	
 }
--- examples/SpringMVC/src/main/webapp/WEB-INF/pages/hello.jsp
+++ examples/SpringMVC/src/main/webapp/WEB-INF/pages/hello.jsp
@@ -3,7 +3,8 @@
 <META HTTP-EQUIV="refresh" CONTENT="1;URL=/SpringMVC/city/${city}">
 </head>
 <body>
-	<h1>Report: Employees living in ${city}</h1>	
+	<h1>Very Important Enterprise Application</h1>	
+	<h2>Report: Employees living in ${city}</h2>	
 	<h2>List : ${empsRes}</h2>	
 </body>
 </html>
