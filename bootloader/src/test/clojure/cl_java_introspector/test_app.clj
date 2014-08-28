6cf6b2bf7f6f68bc78b51fb6dbc5658ed62c3a08
Mathieu Gauthron <mathieu@matlux.net>
2014-08-28 11:46:03 +0100

    issue #8: Added proper test of the NreplServerSpring using ClassPathXmlApplicationContext

--- bootloader/src/test/clojure/cl_java_introspector/test_app.clj
+++ bootloader/src/test/clojure/cl_java_introspector/test_app.clj
@@ -28,6 +28,26 @@
 (def fixture-test1-result (list 'net.matlux.NreplServerSpring 'net.matlux.NreplServer nil nil 'net.matlux.testobjects.Address nil nil ["employee1" "a_test_obj"] '(var user/employee1) Fixtures/EMPLOYEE_FNAME1 Fixtures/STREET1  nil Fixtures/CITY1))
 (def fixture-test1-result-dropped (list ["employee1" "a_test_obj"] '(var user/employee1) Fixtures/EMPLOYEE_FNAME1 Fixtures/STREET1  nil Fixtures/CITY1))
 
+(def test2 (str "(import '(net.matlux NreplServerSpring))
+   (import '(net.matlux NreplServer))
+   (use 'cl-java-introspector.spring)
+   (use 'cl-java-introspector.core)
+   (import 'net.matlux.testobjects.Address)
+   (use 'clojure.reflect 'clojure.pprint 'clojure.java.javadoc)
+   (use 'me.raynes.fs)
+   (get-beans)
+   (get-objs)
+   (def employees (->> (get-obj \"department\") .getEmployees (into []) (map #(vector (keyword (.getFirstname %)) %)) (into {})))
+   (->> (:Mick employees) .getAddress .getCity)
+   (def new-addr (proxy [Address] [\"1 Madison Square\",\"SW2\",\"NY\"] (getStreet [] \"1 Madison Square\") (getCity [] \"NY\")))
+   (.getCity new-addr)
+   (.setAddress (:Mick employees) new-addr)
+   (->> (:Mick employees) .getAddress .getCity)"))
+
+(def fixture-test2-result (vector 'net.matlux.NreplServerSpring 'net.matlux.NreplServer nil nil 'net.matlux.testobjects.Address nil nil ["address1" "address2" "address3" "employee1" "employee2" "employee3" "department" "repl"] [] '(var user/employees) "1 Madison Square" '(var user/new-addr) "NY" nil "NY"))
+(def fixture-test2-result-7dropped (vector ["address1" "address2" "address3" "employee1" "employee2" "employee3" "department" "repl"] [] '(var user/employees) "1 Madison Square" '(var user/new-addr) "NY" nil "NY"))
+
+
 (defn run-remote-test-code1 [port code]
   (let [server (NreplServer. port)
                  _ (.put server "a_test_obj" "this is a test String.")
@@ -43,14 +63,14 @@
     (is (= (run-remote-test-code1 1114 test1)
            fixture-test1-result-dropped))))
 
-(deftest test-app-with-NreplServerSpring
-  (testing "test"
-    (is (= (let [server (NreplServer. 1113)
-                 _ (.put server "a_test_obj" "this is a test String.")
-                 original-emp (Employee. Fixtures/EMPLOYEE_FNAME1 Fixtures/EMPLOYEE_LNAME1 (Address. Fixtures/STREET1 Fixtures/ZIPCODE1 Fixtures/CITY1))
-                 _ (.put server "employee1" original-emp)
-                 res (remote-execute "localhost" 1113 test1)]
-             (.stop server)
-             (.unregisterMBean server)
-             (drop 7 res))
-           (list ["employee1" "a_test_obj"] '(var user/employee1) Fixtures/EMPLOYEE_FNAME1 Fixtures/STREET1  nil Fixtures/CITY1)))))
+;;(deftest test-app-with-NreplServerSpring
+;;  (testing "test"
+;;    (is (= (let [server (NreplServer. 1113)
+;;                 _ (.put server "a_test_obj" "this is a test String.")
+;;                 original-emp (Employee. Fixtures/EMPLOYEE_FNAME1 Fixtures/EMPLOYEE_LNAME1 (Address. Fixtures/STREET1 Fixtures/ZIPCODE1 Fixtures/CITY1))
+;;                 _ (.put server "employee1" original-emp)
+;;                 res (remote-execute "localhost" 1113 test1)]
+;;             (.stop server)
+;;             (.unregisterMBean server)
+;;             (drop 7 res))
+;;           (list ["employee1" "a_test_obj"] '(var user/employee1) Fixtures/EMPLOYEE_FNAME1 Fixtures/STREET1  nil Fixtures/CITY1)))))
--- bootloader/src/test/java/net/matlux/AppTest.java
+++ bootloader/src/test/java/net/matlux/AppTest.java
@@ -5,6 +5,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import clojure.lang.RT;
 import clojure.lang.Symbol;
@@ -19,19 +20,20 @@ import static org.junit.Assert.*;
  */
 public class AppTest
 {
-	
+    
     @BeforeClass
     public static void oneTimeSetUp() {
         // one-time initialization code   
         USE.invoke(REPL_CLIENT_NS);
         USE.invoke(TEST_APP_NS);
-    	System.out.println("@BeforeClass - oneTimeSetUp");
+        USE.invoke(DATA_NS);
+        System.out.println("@BeforeClass - oneTimeSetUp");
     }
  
     @AfterClass
     public static void oneTimeTearDown() {
         // one-time cleanup code
-    	System.out.println("@AfterClass - oneTimeTearDown");
+        System.out.println("@AfterClass - oneTimeTearDown");
     }
  
     @Before
@@ -43,140 +45,158 @@ public class AppTest
     public void tearDown() {
         System.out.println("@After - tearDown");
     }
-	
+    
     final static private Var USE = RT.var("clojure.core", "use");
     final static private Var DROP = RT.var("clojure.core", "drop");
+    final static private Var FIRST = RT.var("clojure.core", "first");
+    final static private Var REST = RT.var("clojure.core", "rest");
+    final static private Var TYPE = RT.var("clojure.core", "type");
+    final static private Var DIFF = RT.var("clojure.data", "diff");
     final static private Symbol REPL_CLIENT_NS = Symbol.intern("cl-java-introspector.client-example");
     final static private Symbol TEST_APP_NS = Symbol.intern("cl-java-introspector.test-app");
+    final static private Symbol DATA_NS = Symbol.intern("clojure.data");
     final static private Var REMOTE_EXECUTE = RT.var("cl-java-introspector.client-example", "remote-execute");
     final static private Var REMOTE_CODE_FIXTURE = RT.var("cl-java-introspector.test-app", "test1");
     final static private Var REMOTE_CODE_RESULT_FIXTURE = RT.var("cl-java-introspector.test-app", "fixture-test1-result");
+    final static private Var REMOTE_CODE_FIXTURE2 = RT.var("cl-java-introspector.test-app", "test2");
+    final static private Var REMOTE_CODE_RESULT_FIXTURE2 = RT.var("cl-java-introspector.test-app", "fixture-test2-result");
 
 
 
-    private Object remoteConnect2ReplAndRunSomeCommands(int port) {
-    	return REMOTE_EXECUTE.invoke("localhost", port, REMOTE_CODE_FIXTURE.deref());
+    private Object remoteConnect2ReplAndRunSomeCommands(Var codefixture, int port) {
+        return REMOTE_EXECUTE.invoke("localhost", port, codefixture.deref());
     }
     
-    private void canSuccessfullyRunRemoteCommands(int port) {
-    	Object result = remoteConnect2ReplAndRunSomeCommands(1112);
-    	System.out.println("testStartApp result=" + result);
-    	System.out.println("testStartApp expected=" + REMOTE_CODE_RESULT_FIXTURE.deref());
+    private void canSuccessfullyRunRemoteCommands(Var codeFixture,Var expectedResult, int port) {
+        Object result = remoteConnect2ReplAndRunSomeCommands(codeFixture, 1112);
+        System.out.println("testStartApp result=" + result);
+        System.out.println("testStartApp expected=" + expectedResult.deref());
+        System.out.println("diff=" + DIFF.invoke(result, expectedResult.deref()));
 
-    	assertTrue(result.equals(REMOTE_CODE_RESULT_FIXTURE.deref()));
+        assertTrue(result.equals(expectedResult.deref()));
+        //assertTrue(FIRST.invoke(result).equals(FIRST.invoke(expectedResult.deref())));
     }
     private void setupFixtureDataOnServer(NreplServer server) {
-		server.put("a_test_obj", "this is a test String.");
-		System.out.println("Added a_test_obj to server");
-    	Employee originalEmployee = new Employee(EMPLOYEE_FNAME1,EMPLOYEE_LNAME1,new Address(STREET1, ZIPCODE1, CITY1) );
-    	server.put("employee1", originalEmployee);
-    	System.out.println("Added employe1 to server");
+        server.put("a_test_obj", "this is a test String.");
+        System.out.println("Added a_test_obj to server");
+        Employee originalEmployee = new Employee(EMPLOYEE_FNAME1,EMPLOYEE_LNAME1,new Address(STREET1, ZIPCODE1, CITY1) );
+        server.put("employee1", originalEmployee);
+        System.out.println("Added employe1 to server");
     }
     
     private void connectionOnPortRefused(int port) {
-    	assertTrue(remoteConnect2ReplAndRunSomeCommands(port).equals("cannot connect"));
+        assertTrue(remoteConnect2ReplAndRunSomeCommands(REMOTE_CODE_FIXTURE, port).equals("cannot connect"));
     }
 
 
     @Test
     public void testStartApp()
     {
-    	NreplServer server = new NreplServer(1112,false,false,true);
-    	setupFixtureDataOnServer(server);
-
-    	
-    	connectionOnPortRefused(1112);
-    	
-    	
-    	server.start();
-    	
-    	canSuccessfullyRunRemoteCommands(1112);
-
-    	
-    	server.stop();
+        NreplServer server = new NreplServer(1112,false,false,true);
+        setupFixtureDataOnServer(server);
+        connectionOnPortRefused(1112);
+        server.start();
+        canSuccessfullyRunRemoteCommands(REMOTE_CODE_FIXTURE, REMOTE_CODE_RESULT_FIXTURE, 1112);
+        server.stop();
+    }
+   @Test
+    public void testSpringApp()
+    {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring/server-test.xml");
+        canSuccessfullyRunRemoteCommands(REMOTE_CODE_FIXTURE2, REMOTE_CODE_RESULT_FIXTURE2, 1112);
+        NreplServerSpring server = (NreplServerSpring)context.getBean("repl");
+        
+        server.stop();
+        server.unregisterMBean();
+        context.close();
     }
     
     @Test
     public void testAutoStartStopApp()
     {
-    	NreplServer server = new NreplServer(1112,true,false,true);
-    	setupFixtureDataOnServer(server);
-    	
-    	canSuccessfullyRunRemoteCommands(1112);
-    	
-    	server.stop();
-    	connectionOnPortRefused(1112);
+        NreplServer server = new NreplServer(1112,true,false,true);
+        setupFixtureDataOnServer(server);
+        
+        canSuccessfullyRunRemoteCommands(REMOTE_CODE_FIXTURE, REMOTE_CODE_RESULT_FIXTURE, 1112);
+        
+        server.stop();
+        connectionOnPortRefused(1112);
     }
 
     @Test
     public void testAutoRegisterStartStopApp()
     {
-    	NreplServer server = new NreplServer(1112,false,true,true); //start server listening onto port number
-    	setupFixtureDataOnServer(server);
+        NreplServer server = new NreplServer(1112,false,true,true); //start server listening onto port number
+        setupFixtureDataOnServer(server);
 
-    	connectionOnPortRefused(1112);
-    	
-    	server.start();
-    	canSuccessfullyRunRemoteCommands(1112);
-    	
-    	server.stop();
-    	server.unregisterMBean();
-    	
-    	connectionOnPortRefused(1112);
+        connectionOnPortRefused(1112);
+        
+        server.start();
+        canSuccessfullyRunRemoteCommands(REMOTE_CODE_FIXTURE, REMOTE_CODE_RESULT_FIXTURE, 1112);
+        
+        server.stop();
+        server.unregisterMBean();
+        
+        connectionOnPortRefused(1112);
     }
     
     @Test
     public void testRegisterStartStopApp()
     {
-    	NreplServer server = new NreplServer(1112,false,false,true); //start server listening onto port number
-    	server.registerMBean();
-    	setupFixtureDataOnServer(server);
+        NreplServer server = new NreplServer(1112,false,false,true); //start server listening onto port number
+        try {
+          server.registerMBean();
+          setupFixtureDataOnServer(server);
 
-    	connectionOnPortRefused(1112);
-    	
-    	server.start();
-    	canSuccessfullyRunRemoteCommands(1112);
-    	
-    	
-    	server.stop();
-    	server.unregisterMBean();
-    	
-    	connectionOnPortRefused(1112);
+          connectionOnPortRefused(1112);
+        
+          server.start();
+          canSuccessfullyRunRemoteCommands(REMOTE_CODE_FIXTURE, REMOTE_CODE_RESULT_FIXTURE, 1112);
+        
+        } finally {
+        
+          server.stop();
+          server.unregisterMBean();
+        
+        }
+        connectionOnPortRefused(1112);
     }
 
     public void testSamePortStartTwice()
     {
-    	NreplServer server = new NreplServer(1113,true,false,true); //start server listening onto port number
-    	NreplServer server2 = new NreplServer(1113,true,false,true); //start server listening onto port number
+        NreplServer server = new NreplServer(1113,true,false,true); //start server listening onto port number
+        NreplServer server2 = new NreplServer(1113,true,false,true); //start server listening onto port number
 
-    	server.stop();
-    	server2.stop();
+        server.stop();
+        server2.stop();
     }
     @Test
     public void testStartTwice()
     {
-    	NreplServer server = new NreplServer(1113); //start server listening onto port number
-    	assertEquals(1113, server.getPort());
-    	server.start();
-    	server.stop();
-    	server.unregisterMBean();
+        NreplServer server = new NreplServer(1113); //start server listening onto port number
+        assertEquals(1113, server.getPort());
+        server.start();
+        server.stop();
+        server.unregisterMBean();
     }
     @Test
     public void testStopTwice()
-    {	
-    	NreplServer server = new NreplServer(1113); //start server listening onto port number
-    	assertEquals(1113, server.getPort());
-    	server.stop();
-    	server.stop();
-    	server.unregisterMBean();
+    {    
+        NreplServer server = new NreplServer(1113); //start server listening onto port number
+        assertEquals(1113, server.getPort());
+        server.stop();
+        server.stop();
+        server.unregisterMBean();
     }
    
     @Test
     public void testStartStopServerThreadSafety1()
-    {	//todo: start multiple servers from different threads
-    	NreplServer server = new NreplServer(1113); //start server listening onto port number
-    	assertEquals(1113, server.getPort());
-    	server.stop();
-    	server.unregisterMBean();
+    {    //todo: start multiple servers from different threads
+        NreplServer server = new NreplServer(1113); //start server listening onto port number
+        assertEquals(1113, server.getPort());
+        server.stop();
+        server.unregisterMBean();
     }
 }
+
+
